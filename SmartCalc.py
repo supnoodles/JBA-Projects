dc61dddd5db6f9a81e5d757db57ace977bd7349b
Dan Y <supnoodless@gmail.com>
2022-04-21 22:17:12 +0100

    refactor: made code more readable upto line 150

--- SmartCalc.py
+++ SmartCalc.py
@@ -1,42 +1,56 @@
+
 class SmartCalc:
+
     def __init__(self):
         self.digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
         self.operators = {'+', '-', '*', '/', '^'}
         self.var_storer = {}
 
-    # assignment --> after the equals sign
-    def assignment_check(self, before_eq, after_eq):
-        if not after_eq or any(x in ['=', '*', '/', '(', ')', '^'] for x in after_eq):
+    def assignment_check(self, before_eq, after_eq) -> None:
+        """
+        Checks that the RHS (after_eq) of an assignment to a variable is valid.
+
+        """
+        self.invalid_assign = {'=', '*', '/', '(', ')', '^'}
+        self.first_char = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '.'}
+        self.op_checker = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'}
+
+        # Sanitise Input after the equals sign.
+        # After equals sign is a variable reference.
+        if (not after_eq or any(x in after_eq for x in self.invalid_assign)) or\
+            (after_eq[0].isalpha() and not after_eq.isalpha()):
             print("Invalid assignment")
-            return
-        elif after_eq[0].isalpha() and not after_eq.isalpha():
-            print("Invalid assignment")
-            return
         elif after_eq.isalpha():
             if after_eq in self.var_storer:
-                self.var_storer[before_eq] = float(self.var_storer[after_eq])
-                return
+                self.var_storer[before_eq] = self.var_storer[after_eq]
             else:
                 print("Unknown variable")
-                return
-        elif after_eq[0] in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '.'}:
-            if len(after_eq) == 1 and after_eq not in {'-', '+', '.'}:
-                self.var_storer[before_eq] = float(after_eq)
-                return
-            elif len(after_eq) == 1 and after_eq in {'-', '+', '.'} or after_eq.count('.') > 1:
-                print('Invalid assignment')
-                return
-            elif 0 <= after_eq.count('.') <= 1:
-                for i, num in enumerate(after_eq[1:]):
-                    if num not in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'}:
-                        print("Invalid assignment")
-                        return
-                    elif num in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'} and i == len(after_eq[1:]) - 1:
-                        self.var_storer[before_eq] = float(after_eq)
-                        return
-
+        # After equals sign is an operation.
+        elif after_eq[0] in self.first_char:
+            if len(after_eq) == 1:
+                if after_eq in {'-', '+', '.'}:
+                    print("Invalid assignment")
+                else:
+                    self.var_storer[before_eq] = float(after_eq)
+            else:
+                if after_eq.count('.') > 1:
+                    print("Invalid assignment")
+                else:
+                    for i, _char in enumerate(after_eq[1:]):
+                        if _char not in self.op_checker:
+                            print("Invalid assignment")
+                            return
+                        elif i == len(after_eq[1:]) - 1:
+                            self.var_storer[before_eq] = float(after_eq)
+            
     @staticmethod
-    def is_valid_brackets(user_input):
+    def is_valid_brackets(user_input: str) -> bool:
+        """
+        For a given string, checks whether:
+        1) There are an equivalent number of open and closed brackets
+        2) If they are used in a correct order. e.g.  )( does not make sense.
+        """
+
         if user_input.count('(') != user_input.count(')'):
             return False
         # Checks if brackets are used in proper order
@@ -47,35 +61,42 @@ class SmartCalc:
                 return False
         return True
 
-    def to_single_operators(self, user_input):
-        end = len(user_input)
-
+    def to_single_operators(self, user_input: str) -> str:
+        """
+        For a given string, this function:
+        1) Removes operators from the end of the string
+        2) Converts multiple +'s & -'s into a single operator. e.g. -- -> + & +- -> -.
+        3) Removes any whitespace.
+        """
+        
         # remove operators from the end
         while user_input[-1] in self.operators:
             user_input = user_input[:-1]
-        # converts consecutive +'s and -'s to single operator
-        for x in range(len(user_input)):
-            if x + 2 <= end:
-                first = user_input[x]
-                second = user_input[x + 1]
-                if (first == '+' and second == '+') or (first == '-' and second == '-'):
-                    user_input = user_input[0:x] + ' +' + user_input[x + 2:end]
-                elif (first == '+' and second == '-') or (first == '-' and second == '+'):
-                    user_input = user_input[0:x] + ' -' + user_input[x + 2:end]
+        # converts consecutive +'s and -'s to single operator  
+        end = len(user_input)
+        for x in range(len(user_input) - 1):
+            curr = user_input[x]
+            next = user_input[x + 1]
+            if (curr == '+' and next == '+') or (curr == '-' and next == '-'):
+                user_input = user_input[0:x] + ' +' + user_input[x + 2:end]
+            elif (curr == '+' and next == '-') or (curr == '-' and next == '+'):
+                user_input = user_input[0:x] + ' -' + user_input[x + 2:end]
         return user_input.replace(' ', '')
 
-    # -------- Infix to Postfix Conversion Algorithm ---------------
-    # 1. Add operands (numbers and variables) to the result (postfix notation) as they arrive.
-    # 2. If the stack is empty or contains a left parenthesis on top, push the incoming operator on the stack.
-    # 3. If the incoming operator has higher precedence than the top of the stack, push it on the stack.
-    # 4. If the incoming operator has lower or equal precedence than or to the top of the stack,
-    #    pop the stack and add operators to the result until you see an operator that has a smaller precedence or
-    #    a left parenthesis on the top of the stack; then add the incoming operator to the stack.
-    # 5. If the incoming element is a left parenthesis, push it on the stack.
-    # 6. If the incoming element is a right parenthesis, pop the stack and add operators to the result until you see
-    #    a left parenthesis. Discard the pair of parentheses.
-    # 7. At the end of the expression, pop the stack and add all operators to the result.
-    def infix_to_postfix(self, infix_str):
+    def infix_to_postfix(self, infix_str: str) -> str:
+        """
+        ----------------- Infix to Postfix Conversion Algorithm ---------------
+        1. Add operands (numbers and variables) to the result (postfix notation) as they arrive.
+        2. If the stack is empty or contains a left parenthesis on top, push the incoming operator on the stack.
+        3. If the incoming operator has higher precedence than the top of the stack, push it on the stack.
+        4. If the incoming operator has lower or equal precedence than or to the top of the stack,
+           pop the stack and add operators to the result until you see an operator that has a smaller precedence or
+           a left parenthesis on the top of the stack; then add the incoming operator to the stack.
+        5. If the incoming element is a left parenthesis, push it on the stack.
+        6. If the incoming element is a right parenthesis, pop the stack and add operators to the result until you see
+           a left parenthesis. Discard the pair of parentheses.
+        7. At the end of the expression, pop the stack and add all operators to the result.
+        """
         postfix = ''
         stack = []
         precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
@@ -117,17 +138,24 @@ class SmartCalc:
                     postfix += stack.pop() + ' '
                 stack.pop()
         # Step 7
-        while len(stack) > 0:
-            postfix += stack.pop() + ' '
-        return postfix.strip()
+        stk_len = len(stack)
+        while stk_len > 0:
+            postfix += stack.pop()
+            if stk_len != 1:
+                postfix += ' '
+            stk_len -= 1
 
-    # -------- Calculate result from Postfix Notation ----------
-    # 1. If the incoming element is a number, push it into the stack (the whole number, not a single digit!).
-    # 2. If the incoming element is the name of a variable, push its value into the stack.
-    # 3. If the incoming element is an operator, then pop twice to get two numbers and
-    #      perform the operation; push the result on the stack.
-    # 4. When the expression ends, the number on the top of the stack is a final result.
-    def calculate_result(self, postfix_str):
+        return postfix
+
+    def calculate_result(self, postfix_str: str):
+        """
+        # -------- Calculate result from Postfix Notation ----------
+        # 1. If the incoming element is a number, push it into the stack (the whole number, not a single digit!).
+        # 2. If the incoming element is the name of a variable, push its value into the stack.
+        # 3. If the incoming element is an operator, then pop twice to get two numbers and
+        #      perform the operation; push the result on the stack.
+        # 4. When the expression ends, the number on the top of the stack is a final result.
+        """
         stack = []
         num_storer = ''
 
